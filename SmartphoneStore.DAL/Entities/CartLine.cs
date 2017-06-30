09705596e73367d7f8e521df0fb0097c41a8d3c6
YevhenKuraiev <yevhen.kuraiev@gmail.com>
2017-06-30 16:52:48 +0300

    Adding a Data Access Layer, a shopping cart and unit tests to it

---
+++ SmartphoneStore.DAL/Entities/CartLine.cs
@@ -0,0 +1,14 @@
+﻿using SmartphoneStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartphoneStore.DAL.Entities
+{
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
