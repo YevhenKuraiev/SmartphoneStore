4d445bb8b74df9db516c27058b6d09191391028a
DESKTOP-NDKAJI3\Yevhen <yevhen.kuraiev@gmail.com>
2017-08-03 15:34:10 +0300

    Add AdminController

--- SmartphoneStore.DAL/Entities/Product.cs
+++ SmartphoneStore.DAL/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace SmartphoneStore.DAL.Entities
     public class Product
     {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Введите наименование товара")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите описание")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Введите положительное значение для цены")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Укажите категорию")]
         public string Category { get; set; }
     }
 }
